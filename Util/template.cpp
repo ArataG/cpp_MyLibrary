32b67f024781c7faa86752a5f798eb6c289f2ad4
Shin SUGIHARA <shinsugihara.jb@gmail.com>
2020-08-23 17:27:18 +0900

    create Util directory

---
+++ Util/template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define REP(i,n) for (int i = 0; i <(n); ++i)
+#define REP2(i,x,n) for (int i = x; i <(n); ++i)
+#define ALL(v) v.begin(), v.end()
+#define RALL(v) v.rbegin(), v.rend()
+using namespace std;
+using ll = long long;
+using P = pair<int,int>;
+static const double PI = acos(-1);
+static const int INF = 1e9+7;
+//debug 
+#ifdef _DEBUG
+#define debug(var)  do{cout << #var << " :";view(var);}while(0)
+#else
+#define debug(...)
+#endif
+template<typename T> void view(T e){cout << e << endl;}
+template<typename T> void view(const vector<T>& v){for(const auto& e : v){ cout << e << " "; } cout << endl;}
+template<typename T> void view(const vector<vector<T> >& vv){ for(const auto& v : vv){ view(v); } }
+
+
+int main(){
+  
+  return 0;
+}
