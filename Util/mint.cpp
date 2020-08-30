5765813e309a5c97e6bb22e6a11ca1de489639f5
ArataM <mau8fg@gmail.com>
2020-08-30 22:57:40 +0900

    add mint.cpp

---
+++ Util/mint.cpp
@@ -0,0 +1,59 @@
+/**
+File   :  mint.cpp
+Brief  :  modの計算ができるライブラリ．intの代わりにmintとして宣言することで，他の四則演算と同様に使用することができる.
+Author :  S.SUGIHARA
+Date   :  2020-08-30
+Update :
+*/
+
+/**
+Reference： atcoder　live_library
+Code test： 
+*/
+#include <bits/stdc++.h>
+#define REP(i,n) for (int i = 0; i <(n); ++i)
+#define REP2(i,x,n) for (int i = x; i <(n); ++i)
+#define ALL(v) v.begin(), v.end()
+#define RALL(v) v.rbegin(), v.rend()
+using namespace std;
+using ll = long long;
+using P = pair<int,int>;
+static const double PI = acos(-1);
+static const int INF = 1e9+7;
+
+/*
+  mint mod int
+*/
+const int mod = 1000000007;
+//const int mod = 998244353;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
