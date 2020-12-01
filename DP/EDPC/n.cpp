c1a5a3b6a7fd3a44df911a53d27fc8eb99cf1e44
Arata <mau8fg@gmail.com>
2020-12-01 16:41:21 +0900

    add edcp/n.cpp

--- DP/EDPC/n.cpp
+++ DP/EDPC/n.cpp
@@ -8,25 +8,62 @@ using ll = long long;
 using P = pair<int,int>;
 static const double PI = acos(-1);
 static const int INF = 1e9+7;
-//debug 
-#ifdef _DEBUG
-#define debug(var)  do{cout << #var << " :";view(var);}while(0)
-#else
-#define debug(...)
-#endif
-template<typename T> void view(T e){cout << e << endl;}
-template<typename T> void view(const vector<T>& v){for(const auto& e : v){ cout << e << " "; } cout << endl;}
-template<typename T> void view(const vector<vector<T> >& vv){ for(const auto& v : vv){ view(v); } }
 
 /*
-
-  問題を逆から考える.
-  
-
+  区間dp + 累積和
 */
+
+const int N = 440;
+ll sum[N][N];
+ll dp[N][N];
+ll a[N];
+static const ll LINF = 1e20;
 
 int main(){
   //#define int long long
-  
+  int n;
+  cin  >> n;
+  REP(i,n) cin >> a[i];
+  //累積和
+  REP(l,n){
+    sum[l][l+1] = a[l];
+    REP2(r,l+2,n+1){
+      sum[l][r] = sum[l][r-1] + a[r-1];
+    }
+  }
+
+  //幅が小さい順にとく
+  for(int width = 2; width <= n; width++){
+    for(int l = 0; l+width <= n; l++){
+      int r = l + width;
+      dp[l][r] = LINF;
+      for(int m = l + 1; m < r ; m++){
+        dp[l][r] = min(dp[l][r], dp[l][m] + dp[m][r] + sum[l][r]);
+      }
+    }
+  }
+  cout << dp[0][n] << endl;
   return 0;
 }
+
+
+/*
+- N匹のスライム, 
+
+- 区間DP
+dp[l][r] = [ l, r )のスライムを 1 体にするときの最小コスト
+
+- 初期値
+dp[i][i+1] = 0
+
+- 最終値
+ans = dp[0][n] 
+
+- dp遷移
+dp[l][r] = min(dp[l][m] + dp[m][r] + [l,m)のスライムと[m, r)スライムを合体させるコスト
+
+l < m < rについてminを取る.
+
+- 範囲が小さい順から見る
+
+*/
